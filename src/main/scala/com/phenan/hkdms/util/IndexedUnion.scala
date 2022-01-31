e779f2bcc1444e6baa04317a2474824b52dc5fb3
Kazuhiro Ichikawa <kazuhiro.ichikawa@linecorp.com>
2022-01-31 11:17:59 +0900

    revert indexed union

--- src/main/scala/com/phenan/hkdms/util/IndexedUnion.scala
+++ src/main/scala/com/phenan/hkdms/util/IndexedUnion.scala
@@ -1,28 +1,8 @@
 package com.phenan.hkdms.util
 
-sealed trait IndexedUnion [T <: Tuple] {
-  def value: Tuple.Union[T]
-  def fold[R](functions: Tuple.Map[T, [t] =>> t => R]): R
-}
-
-object IndexedUnion {
-  def apply[T <: Tuple](value: Tuple.Union[T], index: Int): IndexedUnion[T] = {
-    new NonEmpty(value, index)
-  }
-
-  object Nil extends IndexedUnion[EmptyTuple] {
-    def value: Tuple.Union[EmptyTuple] = {
-      throw new RuntimeException("empty union")
-    }
-    def fold[R](functions: Tuple.Map[EmptyTuple, [t] =>> t => R]): R = {
-      throw new RuntimeException("empty union")
-    }
-  }
-
-  class NonEmpty [T <: Tuple] (val value: Tuple.Union[T], index: Int) extends IndexedUnion[T] {
-    def fold[R](functions: Tuple.Map[T, [t] =>> t => R]): R = {
-      val func = functions.productElement(index).asInstanceOf[Tuple.Union[T] => R]
-      func(value)
-    }
+case class IndexedUnion [T <: Tuple] (val value: Tuple.Union[T], index: Int) {
+  def fold[R](functions: Tuple.Map[T, [t] =>> t => R]): R = {
+    val func = functions.productElement(index).asInstanceOf[Tuple.Union[T] => R]
+    func(value)
   }
 }
