c9a3e8a1dbba36ada1eaacf06f5ceadd732735d4
Kazuhiro Ichikawa <kazuhiro.ichikawa@linecorp.com>
2022-02-05 11:58:35 +0900

    rename map to hmap

--- src/main/scala/com/phenan/hkdms/hkd/HKTree.scala
+++ src/main/scala/com/phenan/hkdms/hkd/HKTree.scala
@@ -7,7 +7,7 @@ import com.phenan.hkdms.util.*
 import scala.deriving.Mirror
 
 sealed trait HKTree [R, F[_]] {
-  def map [G[_]](f: [t] => F[t] => G[t]): HKTree[R, G]
+  def hmap [G[_]](f: [t] => F[t] => G[t]): HKTree[R, G]
 
   def fold (using applicative: Applicative[F]): F[R]
 
@@ -15,22 +15,22 @@ sealed trait HKTree [R, F[_]] {
 }
 
 case class HKProduct [R <: Product, F[_]] (hkd: HKD[R, [e] =>> HKTree[e, F]])(using mirror: Mirror.ProductOf[R]) extends HKTree[R, F] {
-  def map [G[_]](f: [t] => F[t] => G[t]): HKProduct[R, G] = {
-    HKProduct(hkd.map([u] => (hkt: HKTree[u, F]) => hkt.map(f)))
+  def hmap [G[_]](f: [t] => F[t] => G[t]): HKProduct[R, G] = {
+    HKProduct(hkd.map([u] => (hkt: HKTree[u, F]) => hkt.hmap(f)))
   }
   def fold (using applicative: Applicative[F]): F[R] = {
     applicative.map(applicative.productAll(hkd.map[F]([u] => (hkt: HKTree[u, F]) => hkt.fold).asTuple))(mirror.fromProduct)
   }
 }
 case class HKMapped [T, R, F[_]] (tree: HKTree[T, F], mapper: T => R) extends HKTree[R, F] {
-  def map [G[_]](f: [t] => F[t] => G[t]): HKMapped[T, R, G] = {
-    HKMapped(tree.map(f), mapper)
+  def hmap [G[_]](f: [t] => F[t] => G[t]): HKMapped[T, R, G] = {
+    HKMapped(tree.hmap(f), mapper)
   }
   def fold (using applicative: Applicative[F]): F[R] = {
     applicative.map(tree.fold)(mapper)
   }
 }
 case class HKValue [R, F[_]] (value: F[R]) extends HKTree[R, F] {
-  def map [G[_]](f: [t] => F[t] => G[t]): HKValue[R, G] = HKValue(f[R](value))
+  def hmap [G[_]](f: [t] => F[t] => G[t]): HKValue[R, G] = HKValue(f[R](value))
   def fold (using applicative: Applicative[F]): F[R] = value
 }
