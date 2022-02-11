e1440093ecb104f620017b671b672f3d4121d440
Kazuhiro Ichikawa <kazuhiro.ichikawa@linecorp.com>
2022-02-11 21:51:05 +0900

    add test for HKForest

--- src/test/scala/com/phenan/hkdms/HKForestSpec.scala
+++ src/test/scala/com/phenan/hkdms/HKForestSpec.scala
@@ -13,13 +13,20 @@ class HKForestSpec extends AnyFunSuite {
 
   case class Printer [T](show: T => String)
 
+  object Printer {
+    val int: HKForest[Int, Printer] = HKLeaf(Printer(_.toString))
+    val string: HKForest[String, Printer] = HKLeaf(Printer(identity))
+
+    def word(string: String): HKForest[Unit, Printer] = HKLeaf(Printer(_ => string))
+  }
+
   given InvariantSemiringal[Printer] = new InvariantSemiringal[Printer] {
     override def pure[A](a: => A): Printer[A] = {
       Printer[A] (value => "")
     }
 
     override def productAll[T <: Tuple](tupleMap: Tuple.Map[T, Printer]): Printer[T] = {
-      Printer[T] (_.intercalate(showFunctionMap(tupleMap), " "))
+      Printer[T] (_.foldMap(showFunctionMap(tupleMap)))
     }
 
     override def sumAll[T <: Tuple](tupleMap: Tuple.Map[T, Printer]): Printer[IndexedUnion[T]] = {
@@ -37,10 +44,40 @@ class HKForestSpec extends AnyFunSuite {
 
   test("Printer / 単純なデータ型 / 名前指定") {
     val profile = HKProduct[UserProfile, Printer] (
-      name = Printer(identity),
-      age = Printer(_.toString)
+      name = Printer.string,
+      age = Printer.int
     )
     val printer = profile.fold
-    assert(printer.show(UserProfile(name = "Name", age = 21)) == "Name 21")
+    assert(printer.show(UserProfile(name = "Name", age = 21)) == "Name21")
+  }
+
+  test("Printer / 直和型 / 名前指定") {
+    import Printer._
+
+    def wrap[T](name: String)(printer: HKForest[T, Printer]): HKForest[T, Printer] = {
+      word(s"$name(") *>: printer :<* word(")")
+    }
+
+    val user = HKSum[User, Printer](
+      wrap("GuestUser") {
+        HKProduct[GuestUser, Printer](
+          guestId = word("guest id: ") *>: int
+        )
+      },
+      wrap("RegisteredUser") {
+        HKProduct[RegisteredUser, Printer](
+          userId = word("user id: ") *>: int :<* word(", "),
+          profile = wrap("UserProfile") {
+            HKProduct[UserProfile, Printer](
+              name = word("name: ") *>: string :<* word(", "),
+              age = word("age: ") *>: int
+            )
+          }
+        )
+      }
+    )
+
+    val printer = user.fold
+    assert(printer.show(RegisteredUser(userId = 1234, profile = UserProfile(name = "user name", age = 35))) == "RegisteredUser(user id: 1234, UserProfile(name: user name, age: 35))")
   }
 }
