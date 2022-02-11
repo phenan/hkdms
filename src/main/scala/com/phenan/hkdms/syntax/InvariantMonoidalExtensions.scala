67b0e6aeb2d01363785da463041832e9776a344b
Kazuhiro Ichikawa <kazuhiro.ichikawa@linecorp.com>
2022-02-11 17:31:40 +0900

    fix productAll

--- src/main/scala/com/phenan/hkdms/syntax/InvariantMonoidalExtensions.scala
+++ src/main/scala/com/phenan/hkdms/syntax/InvariantMonoidalExtensions.scala
@@ -6,6 +6,10 @@ extension [F[_]] (invariantMonoidal: InvariantMonoidal[F]) {
   @annotation.nowarn
   def productAll[T <: Tuple](tupleMap: Tuple.Map[T, F]): F[T] = {
     tupleMap match {
+      case (e: F[_]) *: EmptyTuple => {
+        val ft = invariantMonoidal.imap(e) { _ *: EmptyTuple } { _.head }
+        ft.asInstanceOf[F[T]]
+      }
       case (e: F[_]) *: (es: Tuple.Map[_, F]) => {
         val pair = invariantMonoidal.product(e, productAll(es))
         val tupled = invariantMonoidal.imap(pair) {
